eadf1b0583c0a6bb6fd8d9183b09c3d854a00d23
YUN <canyun6676@gmail.com>
2023-07-29 01:19:18 +0900

    거의 최종

--- front.py
+++ front.py
@@ -16,39 +16,45 @@ def preprocess(image):
     image = tf.image.resize(image, [256,256]) / 255.0
     return image
 
-def predict(data,model):
+import tensorflow as tf
+
+def predict(dataes, model):
     Classes = {
     0:'cat',
     1:'dog',
     2:'wild'
     }
-
     
-    data = preprocess(data)
-    data = np.expand_dims(data, axis=0)
+    data_file = np.array([preprocess(data) for data in dataes])
     
-    pred = model.predict(data)
+    pred = model.predict(data_file)
     pred = np.argmax(pred,axis=1)
 
-    cols = math.ceil(math.sqrt(len(data)))
-    # 필요한 행의 수를 계산
-    rows = math.ceil(len(data) / cols)
+    cols = math.ceil(math.sqrt(len(dataes)))
+    rows = math.ceil(len(dataes) / cols)
 
-    fig =plt.figure(figsize=(10, 10))
-    for i in range(len(data)):
-        plt.subplot(rows, cols, i + 1)
-        plt.imshow((data[i]*255).astype(np.uint8))
-        plt.title(Classes[pred[i]],fontsize=40)
-        plt.axis("off")
+    fig, axs = plt.subplots(rows, cols, figsize=(10, 10))
+
+    axs = axs.reshape(-1)
+
+    for i, ax in enumerate(axs[:len(dataes)]):
+        # Use tf.cast instead of asType
+        img = tf.cast(data_file[i]*255, tf.uint8)
+        ax.imshow(img.numpy()) # convert tensor to numpy before plotting
+        ax.set_title(Classes[pred[i]], fontsize=40)
+        ax.axis("off")
+    
+    if len(dataes) < len(axs):
+        for ax in axs[len(dataes):]:
+            fig.delaxes(ax)
+
     plt.tight_layout()
     st.pyplot(fig)
-    
-    
+
 def main():
     st.write('# Animals Classification')
-    img_file = st.file_uploader('## 분류할 동물사진을 업로드 하세요.',type=['png','jpg','jpeg'],accept_multiple_files=True)
+    img_files = st.file_uploader('## 분류할 동물사진을 업로드 하세요.',type=['png','jpg','jpeg'],accept_multiple_files=True)
     stream = st.file_uploader('TF.Keras model file (.h5py.zip)', type='zip')
-    print(type(stream))
     if stream is not None:
         myzipfile = zipfile.ZipFile(stream)
         with tempfile.TemporaryDirectory() as tmp_dir:
@@ -58,12 +64,14 @@ def main():
             with keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
                 model = tf.keras.models.load_model(model_dir)
         
-    if img_file is not None:
-        img = Image.open(img_file)
-        img_array = np.array(img)
+    if img_files is not None:
+        img_array = []
+        for img_file in img_files:
+            img = Image.open(img_file)
+            img_array.append(np.array(img))
     
-    if img_file is not None and stream is not None:
-        predict(img,model)  
+    if img_files is not None and stream is not None:
+        predict(img_array,model)  
         
 if __name__ == '__main__':
     main()
