a6755b9d16b08488d272e823b9589e6b6b171df6
YUN <canyun6676@gmail.com>
2023-07-29 00:45:24 +0900

    5

--- front.py
+++ front.py
@@ -16,7 +16,7 @@ def preprocess(image):
     image = tf.image.resize(image, [256,256]) / 255.0
     return image
 
-def predict(data,model_img):
+def predict(data,model):
     Classes = {
     0:'cat',
     1:'dog',
@@ -26,8 +26,7 @@ def predict(data,model_img):
     
     data = preprocess(data)
     data = np.expand_dims(data, axis=0)
-    with keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
-        model = model_img
+    
     pred = model.predict(data)
     pred = np.argmax(pred,axis=1)
 
@@ -49,14 +48,15 @@ def main():
     st.write('# Animals Classification')
     img_file = st.file_uploader('## 분류할 동물사진을 업로드 하세요.',type=['png','jpg','jpeg'])
     stream = st.file_uploader('TF.Keras model file (.h5py.zip)', type='zip')
+    print(type(stream))
     if stream is not None:
         myzipfile = zipfile.ZipFile(stream)
         with tempfile.TemporaryDirectory() as tmp_dir:
             myzipfile.extractall(tmp_dir)
-        root_folder = myzipfile.namelist()[0] # e.g. "model.h5py"
-        model_dir = os.path.join(tmp_dir, root_folder)
-        #st.info(f'trying to load model from tmp dir {model_dir}...')
-        model = tf.keras.models.load_model(model_dir)
+            root_folder = myzipfile.namelist()[0]
+            model_dir = os.path.join(tmp_dir, root_folder)
+            with keras.utils.custom_object_scope({'KerasLayer': hub.KerasLayer}):
+                model = tf.keras.models.load_model(model_dir)
         
     if img_file is not None:
         img = Image.open(img_file)
@@ -64,6 +64,7 @@ def main():
     
     if img_file is not None and stream is not None:
         predict(img,model)  
-
-main()
+        
+if __name__ == '__main__':
+    main()
     
