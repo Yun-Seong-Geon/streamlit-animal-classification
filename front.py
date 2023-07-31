4f426d3a665e846252b0871fffe9804529587a75
YUN <canyun6676@gmail.com>
2023-07-31 13:27:58 +0900

    모델 순서 및 이미지1개 적용

--- front.py
+++ front.py
@@ -12,17 +12,15 @@ import zipfile
 import tempfile
 import os
 import requests
-
-
-def preprocess(image):
-    image = tf.image.resize(image, [256,256]) / 255.0
-    return image
-
-
+import time
 
 FILE_ID = "1MN_hfzw78DVWT0JSPauIVLlzy7L4Wr2R"  # 여기에 모델 파일의 Google Drive ID를 입력하세요
 MODEL_PATH = 'model.h5'
 MODEL_URL = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+
+def preprocess(image):
+    image = tf.image.resize(image, [256,256]) / 255.0
+    return image
 
 def loads_model():
     # 파일이 로컬에 없는 경우에만 다운로드합니다
@@ -43,51 +41,61 @@ def predict(dataes):
     1:'dog',
     2:'wild'
     }
-        
+    with st.spinner('인공지능이 빵을 굽고 있어요...'):
+        model = loads_model()  
     data_file = np.array([preprocess(data) for data in dataes])
-    
-    pred = model.predict(data_file)
+    with st.spinner("인공지능 빵이 나오고 있어요...."):
+        pred = model.predict(data_file)
+        time.sleep(2)
     pred = np.argmax(pred,axis=1)
 
-    cols = math.ceil(math.sqrt(len(dataes)))
-    rows = math.ceil(len(dataes) / cols)
-
-    fig, axs = plt.subplots(rows, cols, figsize=(10, 10))
-
-    axs = axs.reshape(-1)
-
-    for i, ax in enumerate(axs[:len(dataes)]):
-        # Use tf.cast instead of asType
-        ax.imshow(data_file[i]) # convert tensor to numpy before plotting
-        ax.set_title(Classes[pred[i]], fontsize=40)
+    if len(dataes) == 1:
+        # Single image case
+        fig, ax = plt.subplots()  # create a new figure with a default 111 subplot
+        ax.imshow(data_file[0])
+        ax.set_title(Classes[pred[0]], fontsize=40)
         ax.axis("off")
-    
-    if len(dataes) < len(axs):
-        for ax in axs[len(dataes):]:
-            fig.delaxes(ax)
+        st.pyplot(fig) 
+    else:
+        # Multiple images case
+        cols = math.ceil(math.sqrt(len(dataes)))
+        rows = math.ceil(len(dataes) / cols)
 
-    plt.tight_layout()
-    st.pyplot(fig)
+        fig, axs = plt.subplots(rows, cols, figsize=(10, 10))
 
+        axs = axs.reshape(-1)
+
+        for i, ax in enumerate(axs[:len(dataes)]):
+            ax.imshow(data_file[i]) 
+            ax.set_title(Classes[pred[i]], fontsize=40)
+            ax.axis("off")
+        
+        if len(dataes) < len(axs):
+            for ax in axs[len(dataes):]:
+                fig.delaxes(ax)
+
+        plt.tight_layout()
+        st.pyplot(fig)
 def main():
-    st.write('# Animals Classification')
+    st.title('Animals Classification')
     img_files = st.file_uploader('## 분류할 동물사진을 업로드 하세요.',type=['png','jpg','jpeg'],accept_multiple_files=True)
  
+ 
         
-    if img_files is not None:
+    if len(img_files) > 0:
+        
+        
         img_array = []
         for img_file in img_files:
             img = Image.open(img_file)
             img_array.append(np.array(img))
         try:
-            with st.spinner("인공지능 빵 나오는 중...."):
-                predict(img_array)  
+            predict(img_array)  
         except:
             pass
 
    
 if __name__ == '__main__':
-    with st.spinner("인공지능 빵 굽는중...."):
-        model=loads_model()
+    
     main()
     
